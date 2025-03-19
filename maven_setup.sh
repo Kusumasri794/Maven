6f62215e63f59536fdd2135fc8100edaa9ceb292
root <root@ip-172-31-9-5.ec2.internal>
2025-03-19 05:37:14 +0000

    added MAven installation steps

---
+++ maven_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+yum install git java-1.8.0-openjdk maven tree -y
+git clone https://github.com/devopsbyraham/jenkins-java-project.git
+cd jenkins-java-project.git
+
+
+
+
+## Below are the steps to run maven project
+#
+/*
+mvn compile
+mvn test
+mvn package
+mvn install
+mvn clean package
+mvn clean
+
+*/
